1619830ad7f399917e36985b4dde09e964bb4f20
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-17 07:31:09 +0000

    Añadiendo tablas a la base

---
+++ database/migrations/2019_10_17_073046_create_table__productos.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProductos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table__productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table__productos');
+    }
+}
