73ba4f39422ab7e15a7e1416c7817151f9d2b21b
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-17 11:15:21 +0200

    Sigo intentando solucionar errores una vez mas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    use Illuminate\Support\Facades\Schema; 
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //
     }
 }
