ac4ba07bd3ce28c22156a4e3af48d4169816c92a
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-10-18 12:43:56 +0200

    actualizar todos los archivos

--- app/Http/Controllers/workerControler.php
+++ app/Http/Controllers/workerControler.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class workerControler extends Controller
 {
-    //
+    public function recibir_id_tienda($respuesta){
+        $Tienda_ID = $respuesta->method();
+        if($respuesta->method('post')){
+            echo "recibo el ID de la tienda";
+        }
+    }
 }
