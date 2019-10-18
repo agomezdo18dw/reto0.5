65f569caa18d4996df37c3d93c5412907cfe6f95
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-18 09:39:07 +0200

    Cambio de permisos para poder editar y cambio de estilos en el index

--- public/index.php
+++ public/index.php
@@ -7,28 +7,30 @@
 		<script src="js/Diccionario.js"></script>
 	</head>
 	<body>
+		<header>
+			<img id="logo" src="imagenes/Logo.png">
+			<div id="RRSS">
+				<a href="https://www.facebook.com/LaMoreaCC" target="_BLANK"><img class="rrss" src="imagenes/RRSS/facebook.png"></a>
+				<a href="https://twitter.com/la_morea" target="_BLANK"><img class="rrss" src="imagenes/RRSS/twitter.png"></a>
+				<a href="https://www.youtube.com/channel/UCzpwdk2Fe9SwLYByWiZQZ1Q" target="_BLANK"><img class="rrss" src="imagenes/RRSS/youtube.png"></a>
+				<a href="https://www.instagram.com/la_morea/" target="_BLANK"><img class="rrss" src="imagenes/RRSS/instagram.png"></a>
+			</div>
+			<div id="Idiomas">
+				<img class="banderas" src="imagenes/Banderas/Basquecountry_flag.png" onclick="traducir('eus')">
+				<img class="banderas" src="imagenes/Banderas/spain_flag.png" onclick="traducir('esp')">
+				<img class="banderas" src="imagenes/Banderas/united_kingdom_flag.png" onclick="traducir('eng')">
+			</div>
+		</header>
+		<nav>
+			<ul id="Bcliente">
+				<a href="route(/)"><li class="trn" data-trn-key="MInicio">Inicio</li></a>
+				<a href="route(/tiendas)"><li class="trn" data-trn-key="MTiendas">Tiendas</li></a>
+			</ul>
+			<ul id="Btrabajador" >
+				<a href="route(/trabajador)"><li class="trn" data-trn-key="MTrabajador">Trabajador</li></a>
+			</ul>
+		</nav>
 		<section>
-			<header>
-				<img id="logo" src="imagenes/Logo.png">
-				<div id="RRSS">
-					<a href="https://www.facebook.com" target="_BLANK"><img class="rrss" src="imagenes/RRSS/facebook.png"></a>
-					<a href="https://www.twitter.com" target="_BLANK"><img class="rrss" src="imagenes/RRSS/twitter.png"></a>
-					<a href="https://www.youtube.com" target="_BLANK"><img class="rrss" src="imagenes/RRSS/youtube.png"></a>
-					<a href="https://www.instagram.com" target="_BLANK"><img class="rrss" src="imagenes/RRSS/instagram.png"></a>
-				</div>
-				<div id="Idiomas">
-					<img class="banderas" src="imagenes/Banderas/Basquecountry_flag.png" onclick="traducir('eus')">
-					<img class="banderas" src="imagenes/Banderas/spain_flag.png" onclick="traducir('esp')">
-					<img class="banderas" src="imagenes/Banderas/united_kingdom_flag.png" onclick="traducir('eng')">
-				</div>
-			</header>
-			<nav>
-				<ul>
-					<a href="route(/)"><li class="trn" data-trn-key="MInicio">Inicio</li></a>
-					<a href="route(/tiendas)"><li class="trn" data-trn-key="MTiendas">Tiendas</li></a>
-					<a href="route(/trabajador)"><li id="Btrabajador" class="trn" data-trn-key="MTrabajador">Trabajador</li></a>
-				</ul>
-			</nav>
 			<div id="contenido">
 				<div id="infGen">
 					<div id="texto">
@@ -65,14 +67,13 @@
 							</ul>
 						</li>
 					</ul>
-					
 				</div>
 			</div>
-			<footer>
-				<p>
-					Adrián Gómez, Iñigo Perez @ Zubiri manteo
-				</p>
-			</footer>
 		</section>
+		<footer>
+			<p>
+				Adrián Gómez, Iñigo Perez @ Zubiri manteo
+			</p>
+		</footer>
 	</body>
 </html>
