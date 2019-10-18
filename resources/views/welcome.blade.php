d2b0f0bfa4fed31feea4ac5a4de4b074d2403211
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-18 23:05:52 +0200

    Intento de hacer el responsive, mañana sigo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
         @include('includes.nav')    
 		<section>
 			<div id="contenido">
+			<h1>La Morea</h1>
 				<div id="infGen">
 					<div id="texto">
-						<h1>La Morea</h1>
 						<p class="trn" data-trn-key="Parrafo">
 							El Centro Comercial y de Ocio La Morea, abrió sus puertas al público el 16 de octubre del año 2002
 							con el objetivo de poner a disposición de sus clientes la mejor oferta en moda, restauración y ocio de toda la Comunidad Foral de Navarra. Comodidad, sensación de amplitud, ambiente agradable… son algunas de las características básicas que hacen que los visitantes de la Morea se sientan como en casa.
@@ -17,36 +17,34 @@
 					</div>
 					<img id="imgInfGen" src="img/imagenPrincipal.jpg">
 				</div>
-				<div class="mapaInfo">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93701.97160905186!2d-1.7015418568523508!3d42.78440465933851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5091be0c678881%3A0x2943da9e1fdb0643!2sCentro%20Comercial%20La%20Morea!5e0!3m2!1ses!2ses!4v1571226491676!5m2!1ses!2ses"></iframe>
-				</div>
-				<div id="info" class="mapaInfo">
-					<h2 class="trn" data-trn-key="InfTitulo">Información General:</h2>
-					<ul>
-						<li><strong class="trn" data-trn-key="InfNumero">Numero Tlf:</strong>
-							<ul class="subinfo">
-								<li>948 243 796</li>
-							</ul>
-						</li>
-						<li><strong  class="trn" data-trn-key="InfCorreo">Correo electronico:</strong>
-							<ul class="subinfo">
-								<li>info@lamorea.com</li>
-							</ul>
-						</li>
-						<li><strong class="trn" data-trn-key="InfHorario">Horario:</strong>
-							<ul class="subinfo">
-								<li class="trn" data-trn-key="InfDatHorario1">Lunes - Sabado (10:00 - 22:10)</li>
-						    	<li class="trn" data-trn-key="InfDatHorario2">Domingo y festivos (cerrado)</li>
-							</ul>
-						</li>
-					</ul>
+				<div id="mapaInfo">
+					<div id="mapa">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93701.97160905186!2d-1.7015418568523508!3d42.78440465933851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5091be0c678881%3A0x2943da9e1fdb0643!2sCentro%20Comercial%20La%20Morea!5e0!3m2!1ses!2ses!4v1571226491676!5m2!1ses!2ses"></iframe>
+					</div>
+					<div id="info">
+						<h2 class="trn" data-trn-key="InfTitulo">Información General:</h2>
+						<ul id="infoUL">
+							<li><strong class="trn" data-trn-key="InfNumero">Numero Tlf:</strong>
+								<ul class="subinfo">
+									<li>948 243 796</li>
+								</ul>
+							</li>
+							<li><strong  class="trn" data-trn-key="InfCorreo">Correo electronico:</strong>
+								<ul class="subinfo">
+									<li>info@lamorea.com</li>
+								</ul>
+							</li>
+							<li><strong class="trn" data-trn-key="InfHorario">Horario:</strong>
+								<ul class="subinfo">
+									<li class="trn" data-trn-key="InfDatHorario1">Lunes - Sabado (10:00 - 22:10)</li>
+									<li class="trn" data-trn-key="InfDatHorario2">Domingo y festivos (cerrado)</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</section>
-		<footer>
-			<p>
-				Adrián Gómez, Iñigo Perez @ Zubiri manteo
-			</p>
-		</footer>
+		@include('includes.footer')
 	</body>
 </html>
