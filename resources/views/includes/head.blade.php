45db8cdff6100bd55e04eb40c168a67fbe8d38b2
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-10-18 12:18:56 +0200

    he añadido los include a todas las vistas

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,6 @@
 <head>
 	<title>La Morea</title>
-    <link href="{{ URL::asset('s/jquery.translate.js') }}" rel="script">
+    <link href="{{ URL::asset('js/jquery.translate.js') }}" rel="script">
     <link href="{{ URL::asset('js/jquery.js') }}" rel="script">
     <link href="{{ URL::asset('js/Diccionario.js') }}" rel="script">
     <link rel="stylesheet" href="{{ URL::asset('/css/header.css') }}">
--- resources/views/trabajador.blade.php
+++ resources/views/trabajador.blade.php
@@ -1,35 +1,8 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>La Morea/Trabajador</title>
-		<link rel="stylesheet" type="text/css" href="estilos/index.css">
-		<script src="js/jquery.js"></script>
-		<script src="js/jquery.translate.js"></script>
-		<script src="js/Diccionario.js"></script>
-	</head>
-	<body>
-		<header>
-			<img id="logo" src="imagenes/Logo.png">
-			<div id="RRSS">
-				<a href="https://www.facebook.com/LaMoreaCC" target="_BLANK"><img class="rrss" src="imagenes/RRSS/facebook.png"></a>
-				<a href="https://twitter.com/la_morea" target="_BLANK"><img class="rrss" src="imagenes/RRSS/twitter.png"></a>
-				<a href="https://www.youtube.com/channel/UCzpwdk2Fe9SwLYByWiZQZ1Q" target="_BLANK"><img class="rrss" src="imagenes/RRSS/youtube.png"></a>
-				<a href="https://www.instagram.com/la_morea/" target="_BLANK"><img class="rrss" src="imagenes/RRSS/instagram.png"></a>
-			</div>
-			<div id="Idiomas">
-				<img class="banderas" src="imagenes/Banderas/Basquecountry_flag.png" onclick="traducir('eus')">
-				<img class="banderas" src="imagenes/Banderas/spain_flag.png" onclick="traducir('esp')">
-				<img class="banderas" src="imagenes/Banderas/united_kingdom_flag.png" onclick="traducir('eng')">
-			</div>
-		</header>
-		<nav>
-			<ul id="Bcliente">
-				<a href="<?php echo route('/'); ?>"><li class="trn" data-trn-key="MInicio">Inicio</li></a>
-				<a href="<?php echo route('tiendas'); ?>"><li class="trn" data-trn-key="MTiendas">Tiendas</li></a>
-			</ul>
-			<ul id="Btrabajador" >
-				<a href="<?php echo route('trabajador'); ?>"><li class="trn" data-trn-key="MTrabajador">Trabajador</li></a>
-			</ul>
-		</nav>
+@include('includes.head')
+        @include('includes.header')
+        @include('includes.nav')
 		<section>
 			<div id="seleccionTienda">
                 <h2>En que tienda trabajas?</h2>
