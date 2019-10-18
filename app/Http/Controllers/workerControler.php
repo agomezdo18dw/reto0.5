4a779d0e9bf6d63ebbba5c31fb4e9b7614f94c72
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-10-18 13:54:28 +0200

    actualizado a ultima versino de codigo

--- app/Http/Controllers/workerControler.php
+++ app/Http/Controllers/workerControler.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class workerControler extends Controller
 {
     public function recibir_id_tienda(Request $respuesta){
-            $Tienda_ID = $respuesta->input("Tiendas");
-            return view('tienda',['ID' => $Tienda_ID]); 
+        $ID=$respuesta->input("Tiendas");
+        return view('tienda',['ID' =>$ID]); 
     }
 }
