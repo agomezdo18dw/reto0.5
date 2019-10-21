ff77ec0fe1ebb0e6a36b971b48d2eaccfe899218
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-21 10:27:45 +0200

    intento de ver datos en tiendas y añadidos permisos

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Producutos;
 
 class productController extends Controller
 {
-    //
+    public function recogerProductos(){
+        $productos = Productos::all();
+        return view('tiendas', compact('productos'));
+    }
 }
