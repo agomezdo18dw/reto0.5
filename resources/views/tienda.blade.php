befdaa3f0c3bdb22b2b829abdedd655c76148955
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-10-21 13:31:02 +0200

    añadiendo gestion de productos

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -12,34 +12,34 @@
 		<section>
 			<h1 id="GestionProducto">Gestion de productos</h1>
 			<div id="submenu">
-				<button id="MostrarProductos" >Mostrar Productos</button>
-				<button id="EditarProductos" >Editar Productos</button>
+				<button id="MostrarProductos" href="/MostrarProductos/{$_SESSION['IDtienda']}">Mostrar Productos</button>
+				<button id="InsertarProductos" href="/InsertarProductos/{$_SESSION['IDtienda']}">Insertar Productos</button>
 			</div>	
 			<div id="container">
 				<?php 
 					if(isset($_GET['MostrarProductos'])){
-
+							echo "pulsado mostrar productos";
 					}
-					elseif(isset($_GET['EditarProductos'])){
-						
-							<form action="{{route('IDtienda')}}" method="post">
-								@csrf
-								<label> Nombre: </label>
-									<input type="text" name="NombreProducto" require><br><br>
-								<label> Descripcion: </label>
-									<textarea name="DescripcionProducto" rows="8" cols="30" require>Inserte Descripcion del producto</textarea><br><br>
-								<label>Precio de venta (€): </label>
-									<input type="number" name="PrecioVenta" require><br><br>
-								<label>Precio de compra (€): </label>
-									<input type="number" name="PrecioCompra" require><br><br>
-								<label> Stock: </label>
-									<input type="number" name="Stock" require><br><br>
-								<label> Foto: </label>
-									<input type="file" name="foto" require><br><br>
-								<label> Enlace: </label>
-									<input type="url" name="EnlaceProducto" require><br><br>
-								<input type="submit" name="AñadirProducto" value="Añadir Producto">
-							</form>
+					elseif(isset($_GET['InsertarProductos'])){
+						echo "pulsado insertar productos";
+							//<form action="{{route('IDtienda')}}" method="post">
+							//	@csrf
+							//	<label> Nombre: </label>
+							//		<input type="text" name="NombreProducto" require><br><br>
+							//	<label> Descripcion: </label>
+							//		<textarea name="DescripcionProducto" rows="8" cols="30" require>Inserte Descripcion del producto</textarea><br><br>
+							//	<label>Precio de venta (€): </label>
+							//		<input type="number" name="PrecioVenta" require><br><br>
+							//	<label>Precio de compra (€): </label>
+							//		<input type="number" name="PrecioCompra" require><br><br>
+							//	<label> Stock: </label>
+							//		<input type="number" name="Stock" require><br><br>
+							//	<label> Foto: </label>
+							//		<input type="file" name="foto" require><br><br>
+							//	<label> Enlace: </label>
+							//		<input type="url" name="EnlaceProducto" require><br><br>
+							//	<input type="submit" name="AñadirProducto" value="Añadir Producto">
+							//</form>
 					}
 				?>
 			</div>
