1b333da95a28085822d76a6b51142a56865a9978
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-22 11:50:49 +0200

    conflictos

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -15,29 +15,7 @@
 				<button name="InsertarProductos"><a href="">Insertar Producto</a></button>
 			</div>	
 			<div id="container">
-				<?php 
-					if(isset($_POST['InsertarProductos'])){
-						echo "pulsado insertar productos";
-							//<form action="{{route('IDtienda')}}" method="post">
-							//	@csrf
-							//	<label> Nombre: </label>
-							//		<input type="text" name="NombreProducto" require><br><br>
-							//	<label> Descripcion: </label>
-							//		<textarea name="DescripcionProducto" rows="8" cols="30" require>Inserte Descripcion del producto</textarea><br><br>
-							//	<label>Precio de venta (€): </label>
-							//		<input type="number" name="PrecioVenta" require><br><br>
-							//	<label>Precio de compra (€): </label>
-							//		<input type="number" name="PrecioCompra" require><br><br>
-							//	<label> Stock: </label>
-							//		<input type="number" name="Stock" require><br><br>
-							//	<label> Foto: </label>
-							//		<input type="file" name="foto" require><br><br>
-							//	<label> Enlace: </label>
-							//		<input type="url" name="EnlaceProducto" require><br><br>
-							//	<input type="submit" name="AñadirProducto" value="Añadir Producto">
-							//</form>
-					}
-				?>
+				
 				<div class="productos">
 						@foreach($productos as $producto)
 							@if($tienda->ID_Tienda === $producto->ID_Tienda)
@@ -47,8 +25,6 @@
 									<h4>{{$producto->Stock}}</h4>
 									@if($producto->Stock === 0)
 										<p class="Agotado">AGOTADO</p>
-									@else
-										<button>Comprar</button>
 									@endif
 								</div>
 							@endif	
