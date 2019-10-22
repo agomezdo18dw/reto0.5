215c0f287deb41755566e15433c7eca5c3c279ff
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-22 17:49:27 +0200

    solucionado la vision de imagenes del header en el formulario

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,14 +1,14 @@
 <header>
-    <img id="logo" src="img/Logo.png">
+    <img id="logo" src="/img/Logo.png">
     <div id="RRSS">
-	    <a href="https://www.facebook.com/LaMoreaCC" target="_BLANK"><img class="rrss" src="img/RRSS/facebook.png"></a>
-	    <a href="https://twitter.com/la_morea" target="_BLANK"><img class="rrss" src="img/RRSS/twitter.png"></a>
-	    <a href="https://www.youtube.com/channel/UCzpwdk2Fe9SwLYByWiZQZ1Q" target="_BLANK"><img class="rrss" src="img/RRSS/youtube.png"></a>
-	    <a href="https://www.instagram.com/la_morea/" target="_BLANK"><img class="rrss" src="img/RRSS/instagram.png"></a>
+	    <a href="https://www.facebook.com/LaMoreaCC" target="_BLANK"><img class="rrss" src="/img/RRSS/facebook.png"></a>
+	    <a href="https://twitter.com/la_morea" target="_BLANK"><img class="rrss" src="/img/RRSS/twitter.png"></a>
+	    <a href="https://www.youtube.com/channel/UCzpwdk2Fe9SwLYByWiZQZ1Q" target="_BLANK"><img class="rrss" src="/img/RRSS/youtube.png"></a>
+	    <a href="https://www.instagram.com/la_morea/" target="_BLANK"><img class="rrss" src="/img/RRSS/instagram.png"></a>
     </div>
     <div id="Idiomas">
-        <img class="banderas" src="img/Banderas/united_kingdom_flag.png" onclick="traducir('eng')" id="inglesa">				
-	    <img class="banderas" src="img/Banderas/spain_flag.png" onclick="traducir('esp')">
-        <img class="banderas" src="img/Banderas/Basquecountry_flag.png" onclick="traducir('eus')">
+        <img class="banderas" src="/img/Banderas/united_kingdom_flag.png" onclick="traducir('eng')" id="inglesa">				
+	    <img class="banderas" src="/img/Banderas/spain_flag.png" onclick="traducir('esp')">
+        <img class="banderas" src="/img/Banderas/Basquecountry_flag.png" onclick="traducir('eus')">
     </div>
 </header>
