de85d9f6ea45a737a73deb517bc05dea9f7a09f3
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-24 13:47:30 +0200

    Intento de modificacion de stock

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,6 @@ Route::post('tienda/{id}','productController@vueltaTienda')->name('tienda2');
 
 Route::post('editar/{idproducto}','productController@editarProducto')->name('editar');
 
+Route::post('modificar/{idproducto}','productController@modificarProducto')->name('modificar');
+
 Route::post('eliminar/{idproducto}','productController@eliminarProducto');
