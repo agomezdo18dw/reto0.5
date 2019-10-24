2a565e627d3a0261e4f64633a00a7675d34fa2c6
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-10-24 11:18:11 +0200

    Añadir a DatabaseSeeder que llame a LaMoreaSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(LaMoreaSeeder::class);
         $this->call(TiendasSeeder::class);
         $this->call(ProductosSeeder::class);
     }
